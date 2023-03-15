207d1f34b4ba9612e9740fd49ef43547d726e706
IlariaGonchiar <125513828+IlariaGonchiar@users.noreply.github.com>
2023-03-15 22:13:38 +0200

    Add files via upload

---
+++ len.py
@@ -0,0 +1,11 @@
+A = [3.5, 2.8, 'abc', [ 2, 3, False]]
+lenght = len(A)
+print("lenght = ", lenght)
+
+B = ["Hello world!"]
+lenght = len(B)
+print("lenght = ", lenght)
+
+C = [ 0, 3, 2, 4, 7 ]
+lenght = len(C)
+print("lenght = ", lenght)
---
+++ нумерація списку.py
@@ -0,0 +1,4 @@
+myList=[2.5, 8, "Hello"]
+print(myList[0])
+print(myList[1])
+print(myList[2])
